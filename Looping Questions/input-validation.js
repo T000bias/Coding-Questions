f7ae55aef47fc1f9825eb0892d6ecb0d6654f784
Rodney Foster Jr <rodney.fosterbfhs@gmail.com>
2022-06-04 20:29:05 -0400

    Finished the fizzbuzz question

---
+++ Looping Questions/input-validation.js
@@ -0,0 +1,8 @@
+/*
+Write a program that continues to ask the user for a number until the entered number
+is less than or equal to 100.
+
+When you are done with the above, imporve the program so that the terminating number
+is between 50 and 100.
+*/
+
---
+++ Looping Questions/multiplication-table.js
@@ -0,0 +1,7 @@
+/* 
+Write a program that asks the user for a number, then shows the multiplication table 
+for this number.
+
+When you are done, improve the program so it only accepts numbers between 2 and 9
+(use the input validation exercise)
+*/
